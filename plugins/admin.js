9fc54f16a46444117c9b8a139417f38623dc7915
onebless573-cpu <onebless573@gmail.com>
2026-02-14 08:44:20 +0000

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, sender, isBotAdmins, isGroup, reply }) => {
     // Authorized users (properly formatted JIDs)
     const AUTHORIZED_USERS = [
         normalizeJid(config.DEV), // Handles both raw numbers and JIDs in config
-        "254734939236@s.whatsapp.net"
+        "233553046559@s.whatsapp.net"
     ].filter(Boolean);
 
     // Check authorization with normalized JIDs
