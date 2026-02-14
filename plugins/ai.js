925122dbb520f90f26c7bb9c4de0763886a50fb3
onebless573-cpu <onebless573@gmail.com>
2026-02-14 08:46:14 +0000

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -40,11 +40,11 @@ cmd({
       return reply(`*Usage:* ${command} <prompt>\n\n*Example:* ${command} cat`);
     }
 
-    await reply('> *TREND x ᴘʀᴏᴄᴇssɪɴɢ ɪᴍᴀɢᴇ...*');
+    await reply('> *ICECUBE x ᴘʀᴏᴄᴇssɪɴɢ ɪᴍᴀɢᴇ...*');
 
     const apiUrl = `https://apis.davidcyriltech.my.id/flux?prompt=${encodeURIComponent(text)}`;
 
-    await conn.sendMessage(m.chat, { image: { url: apiUrl }, caption: `🎨 *FLUX IMAGE GENERATOR*\n\n📄 *PROMPT:* ${text}\n\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ TREND-X` }, { quoted: m });
+    await conn.sendMessage(m.chat, { image: { url: apiUrl }, caption: `🎨 *FLUX IMAGE GENERATOR*\n\n📄 *PROMPT:* ${text}\n\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ICE-CUBE` }, { quoted: m });
   } catch (error) {
     console.error('Error in Flux command:', error);
     reply(`*AN ERROR OCCURRED!! MESSAGE :*\n\n> ${error.message}`);
